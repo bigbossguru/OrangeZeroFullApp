7260b0ac0eb483fff3350949cd676ad343c07bd3
Eldar Gazizov <kursus1010@gmail.com>
2024-08-18 14:29:00 +0200

    #5 Implemented script for build and run unix services

---
+++ build_run_services.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Path to the root directory
+pwd
+
+# Create and Run all UNIX services
+
+# Backend run gunicorn socket and service
+cd ./backend
+echo "Backend run gunicorn socket and service"
+pwd
+poetry install --sync --no-root
+make create-gunicorn-socket
+make create-gunicorn-service
+make run-gunicorn-server
+
+cd ..
+
+# Configuration Nginx
+cd ./nginx
+echo "Configuration Nginx service"
+pwd
+make config-nginx-proxy
+make restart-nginx
+
+cd ..
+
+# Configuration Ngrok
+cd ./ngrok
+echo "Configuration Ngrok service"
+pwd
+make create-service
+make config-ngrok
+make run-service
+
+cd ..
+
+# Create Telegram Bot
+cd ./telegram_bot
+echo "Create Telegram Bot service"
+pwd
+poetry install --sync --no-root
+make create-service
+make run-service
+
+cd ..
+pwd
+echo "Finished"
